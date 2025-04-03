6dd3a3731f38a969a3fee05709932a5ccf071eb3
NeoAfsenius <neo.afsenius@skola.taby.se>
2025-04-03 18:22:40 +0200

    Senast, left click up and down

--- code.js
+++ code.js
@@ -25,22 +25,47 @@ document.onkeydown = function (e) {
     const key = e.key
     keys[key] = true // Sätter t.ex. keys.w till true
 }
-document.onkeydown = function (e) {
-    console.log(e) //Inparametern e innehåller ett event-objekt med information om eventet.
+
+let isLeftMouseDown = false
+
+document.addEventListener("mousedown", function(event) {
+    if (event.button === 0) { // 0 is the left mouse button
+        console.log("Mouse X:", event.clientX, "Mouse Y:", event.clientY);
+        isLeftMouseDown = true
+        console.log(isLeftMouseDown)
+    }
+})
+
+document.addEventListener("mouseup", function(event) {
+    if (event.button === 0) { // Left mouse button released
+        isLeftMouseDown = false;
+        console.log(isLeftMouseDown);
+    }
+});
+
+document.onkeyup = function (e) {
     const key = e.key
-    keys[key] = true // Sätter t.ex. keys.w till true
-  }
+    keys[key] = false
+}
+
 console.log(
     `Bredd på canvas: ${canvas.width},
   Höjd på canvas: ${canvas.height}`
   .a
   );
+let OnOther=false
+function drawBase() {
+    c.fillStyle = "green"
+    c.fillRect(0,(canvas.height)-(canvas.height/10), canvas.width, canvas.height/10)
+    c.fillStyle = "black"
+    c.fillRect(200, (canvas.height)-(canvas.height/10)-100, 30, 100)
 
-function drawGround() {
-  c.fillStyle = "green"
-  c.fillRect(0,(canvas.height)-(canvas.height/10), canvas.width, canvas.height/10)
+    
+    if (isLeftMouseDown = true && OnOther===false) {
+    }
+    drawBase()
 }
-drawGround()
+drawBase()
 
 document.onkeydown = function (e) {
     console.log(e) //Inparametern e innehåller ett event-objekt med information om eventet.

fc5560d70dc8c0290cb716d3bb762dac20f1527c
NeoAfsenius <neo.afsenius@skola.taby.se>
2025-04-04 17:22:49 +0200

    boll fungerar men vektorer

--- code.js
+++ code.js
@@ -7,14 +7,44 @@ canvas.style.height = "100%";
 
 const c = canvas.getContext("2d");
 
+document.addEventListener("mousedown", function (event) {
+  if (event.button === 0 && ForceMouseDown === false) {
+    // 0 is the left mouse button
+    console.log("Mouse X:", event.clientX, "Mouse Y:", event.clientY);
+    MouseX = event.clientX;
+    MouseY = event.clientY;
+    BalltoMouseX = MouseX - 230;
+    BalltoMousey = MouseY - (canvas.height - canvas.height / 10 - 100);
+    BallToMouseAngle = Math.atan2(BalltoMousey, BalltoMouseX);
+    isLeftMouseDown = true; //left button pressed
+    ForceMouseDown = true;
+  }
+});
+
+document.addEventListener("mouseup", function (event) {
+  if (event.button === 0) {
+    // 0 is the left mouse button
+    // Left mouse button released
+    isLeftMouseDown = false;
+    console.log(isLeftMouseDown);
+    ForceMouseDown = false;
+    BallShot = false;
+  }
+});
+document.addEventListener("mousemove", function (event) {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
+
 class ball {
-  constructor(active, x, y, material, xvelocity, radius, grav /*Neo tyckte det var coolt*/) {
-    this.velocity = -10;
+  constructor(active, x, y, material, radius, grav /*Neo tyckte det var coolt*/, angle) {
+    this.totalVelocity = Math.sqrt(15 ** 2 + 15 ** 2);
+    this.xvelocity = Math.cos(angle) * this.totalVelocity;
+    this.velocity = Math.sin(angle) * this.totalVelocity; //sätt -10 för standard
     this.active = false;
     this.x = x;
     this.y = y;
     this.material = material;
-    this.xvelocity = xvelocity;
     this.radius = radius;
     this.grav = grav;
   }
@@ -22,16 +52,14 @@ class ball {
     //uppdaterar bollen rörelse
     this.velocity += this.grav; // Kör gravitation
     this.y += this.velocity; // Rör bollen ner
-
     let ground = canvas.height - canvas.height / 10 - this.radius;
-
     if (this.y >= ground) {
       this.y = ground;
-      this.velocity = -this.velocity * 0.9;
+      this.velocity = -this.velocity * 0.7;
+      this.xvelocity = this.xvelocity * 0.7;
     }
     this.x += this.xvelocity; // Rör bollen sidleds
-    this.xvelocity = this.xvelocity*0.9999
-
+    this.xvelocity = this.xvelocity * 0.9999;
   }
   draw() {
     c.fillStyle = this.material;
@@ -54,29 +82,6 @@ document.onkeydown = function (e) {
   keys[key] = true; // Sätter t.ex. keys.w till true
 };
 
-document.addEventListener("mousedown", function (event) {
-  if (event.button === 0 && ForceMouseDown===false) {
-    // 0 is the left mouse button
-    console.log("Mouse X:", event.clientX, "Mouse Y:", event.clientY);
-    isLeftMouseDown = true; //left button pressed
-    ForceMouseDown = true
-  }
-})
-
-document.addEventListener("mouseup", function (event) {
-  if (event.button === 0) {
-    // 0 is the left mouse button
-    // Left mouse button released
-    isLeftMouseDown = false;
-    console.log(isLeftMouseDown);
-    ForceMouseDown = false
-    BallShot = false
-  }
-});
-document.addEventListener("mousemove", function (event) {
-  mouse.x = event.clientX;
-  mouse.y = event.clientY;
-});
 //random number generator, mindre matte i koden
 function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -100,7 +105,6 @@ let BallList = [];
 let BallShot = false;
 let ForceMouseDown = false;
 
-
 function drawBase() {
   c.clearRect(0, 0, canvas.width, canvas.height);
   c.fillStyle = "green";
@@ -109,10 +113,10 @@ function drawBase() {
   c.fillStyle = "black";
   c.fillRect(200, canvas.height - canvas.height / 10 - 100, 30, 100);
 
-  if (isLeftMouseDown === true && BallShot===false) {
-    const newBall = new ball(true, 230, canvas.height - canvas.height / 10 - 100, "grey", 10, 5, 0.2);
+  if (isLeftMouseDown === true && BallShot === false) {
+    const newBall = new ball(true, 230, canvas.height - canvas.height / 10 - 100, "grey", 5, 0.2, BallToMouseAngle);
     BallList.push(newBall);
-    BallShot = true
+    BallShot = true;
   }
   BallList.forEach((balls) => {
     balls.update();
